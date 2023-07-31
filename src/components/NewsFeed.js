4ad5f0e682de41e8bbaf62eac2f0e106b52dd101
vedant-3010 <vedantdandgepro@gmail.com>
2023-07-31 05:46:31 +0530

    caching added

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,12 +1,11 @@
 // components/NewsFeed.js
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRandomPhotos } from '../redux/newsFeedSlice';
 import PhotoCard from './PhotoCard';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import LoadingSpinner from './LoadingSpinner';
 import styles from '../styles/NewsFeed.module.css';
-
+import { fetchRandomPhotos, selectIsDataCached } from '../redux/newsFeedSlice';
 const NewsFeed = () => {
   const dispatch = useDispatch();
   const [page, setPage] = useState(1);
@@ -14,9 +13,16 @@ const NewsFeed = () => {
   const hasMore = useSelector((state) => state.newsFeed.hasMore);
   const loading = useSelector((state) => state.newsFeed.loading);
   const error = useSelector((state) => state.newsFeed.error);
-  useEffect(() => {
-    dispatch(fetchRandomPhotos(page));
-  }, [dispatch, page]);
+  
+   // Check if data is cached
+   const isDataCached = useSelector(selectIsDataCached);
+
+   useEffect(() => {
+     // Fetch data only if not cached
+     if (!isDataCached) {
+       dispatch(fetchRandomPhotos(page));
+     }
+   }, [dispatch, page, isDataCached]);
 
   const fetchMorePhotos = () => {
     setPage(page + 1);
@@ -24,6 +30,14 @@ const NewsFeed = () => {
 
   return (
     <div className={styles.newsFeedContainer}>
+      {isDataCached ? (
+        // Render photos from the Redux state if data is cached
+        <div>
+          {photos.map((photo) => (
+            <PhotoCard key={photo.id} photo={photo} />
+          ))}
+        </div>
+      ) : (
       <InfiniteScroll
         dataLength={photos.length}
         next={fetchMorePhotos}
@@ -36,7 +50,15 @@ const NewsFeed = () => {
         ))}
       </InfiniteScroll>
 
-      {error && <p>Error occurred while fetching photos.</p>}
+)}
+
+{error && (
+  <p>
+    {error.response?.status === 403
+      ? 'API rate limit reached. Please try again later.'
+      : 'Error occurred while fetching photos.'}
+  </p>
+)}
     </div>
   );
 };
--- src/redux/newsFeedSlice.js
+++ src/redux/newsFeedSlice.js
@@ -4,49 +4,54 @@ import axios from 'axios';
 
 const API_KEY = 'QnAJ_PJE0cJQPm7z1eLB0Is9kOp_rIOI4jXnnnh_THs'; // Replace with your Unsplash API key
 
-const fetchRandomPhotos = createAsyncThunk(
-    'newsFeed/fetchRandomPhotos',
-    async (page) => {
-      const response = await axios.get(`https://api.unsplash.com/photos/random`, {
-        params: {
-          count: 10,
-          page,
-          client_id: API_KEY,
-        },
-      });
-      return response.data;
-    }
-  );
-  
-  const newsFeedSlice = createSlice({
-    name: 'newsFeed',
-    initialState: {
-      photos: [],
-      loading: false,
-      error: null,
-      hasMore: true, // Initialize hasMore to true
-    },
-    reducers: {},
-    extraReducers: (builder) => {
-      builder
-        .addCase(fetchRandomPhotos.pending, (state) => {
-          state.loading = true;
-        })
-        .addCase(fetchRandomPhotos.fulfilled, (state, action) => {
-          state.photos = [...state.photos, ...action.payload];
-          state.loading = false;
-          state.error = null;
-          if (action.payload.length === 0) {
-            state.hasMore = false; // Set hasMore to false when no more photos are available
-          }
-        })
-        .addCase(fetchRandomPhotos.rejected, (state, action) => {
-          state.loading = false;
-          state.error = action.error.message;
-          state.hasMore = false; // Set hasMore to false in case of an error
-        });
+const CACHE_DURATION = (60 * 1000)*3; // Cache data for 1 minute (in milliseconds)
+
+const fetchRandomPhotos = createAsyncThunk('newsFeed/fetchRandomPhotos', async (page) => {
+  const response = await axios.get(`https://api.unsplash.com/photos/random`, {
+    params: {
+      count: 10,
+      page,
+      client_id: API_KEY,
     },
   });
-  
-  export { fetchRandomPhotos };
-  export default newsFeedSlice.reducer;
+  return response.data;
+});
+
+const newsFeedSlice = createSlice({
+  name: 'newsFeed',
+  initialState: {
+    photos: [],
+    loading: false,
+    error: null,
+    hasMore: true,
+    lastFetchedAt: null, // Track the timestamp of the last API response
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRandomPhotos.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchRandomPhotos.fulfilled, (state, action) => {
+        state.photos = [...state.photos, ...action.payload];
+        state.loading = false;
+        state.error = null;
+        state.hasMore = action.payload.length > 0; // Set hasMore based on the API response
+        state.lastFetchedAt = Date.now(); // Record the timestamp of the last API response
+      })
+      .addCase(fetchRandomPhotos.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+        state.hasMore = false;
+      });
+  },
+});
+
+// Add a selector to check if the data is still within the caching period
+export const selectIsDataCached = (state) => {
+  if (!state.newsFeed.lastFetchedAt) return false;
+  return Date.now() - state.newsFeed.lastFetchedAt < CACHE_DURATION;
+};
+
+export { fetchRandomPhotos };
+export default newsFeedSlice.reducer;
