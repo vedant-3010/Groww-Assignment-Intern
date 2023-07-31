4ee9a4333967c10aecc01681b26b94bed672b9f1
vedant-3010 <vedantdandgepro@gmail.com>
2023-07-31 13:38:07 +0530

    Profile Page Changes

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/PhotoCard.module.css';
+import {
+  AiOutlineHeart,
+  AiOutlineDownload,
+} from "react-icons/ai";
 const PhotoCard = ({ photo }) => {
   const { id, urls, user, likes, downloads, location, alt_description } = photo;
 
   return (
     <div className={styles.photocard}>
-      <div className={styles.userinfo}>
-        <img
-          src={user.profile_image.small}
-          alt={user.name}
-          className={styles.avatar}
-        />
-        <div className={styles.usernamelocation}>
-          <p className={styles.username}>
-            <Link legacyBehavior href={`/user/${user.username}`} passHref>
-              <a>{user.name}</a>
-            </Link>
-          </p>
-          <p className={styles.location}>
-            {location ? location.name : 'Unknown'}
-          </p>
+      <Link legacyBehavior href={`/user/${user.username}`} passHref>
+
+
+        <div className={styles.userinfo}>
+
+          <img
+            src={user.profile_image.small}
+            alt={user.name}
+            className={styles.avatar}
+          />
+          <div className={styles.usernamelocation}>
+            <p className={styles.username}>
+              <Link legacyBehavior href={`/user/${user.username}`} passHref>
+                <a>{user.name}</a>
+              </Link>
+            </p>
+            <p className={styles.location}>
+              {location ? location.name : 'Unknown'}
+            </p>
+          </div>
         </div>
-      </div>
-        <img src={urls.regular} alt={alt_description} className={styles.postimage} />
+      </Link>
+      <img src={urls.regular} alt={alt_description} className={styles.postimage} />
       <div className={styles.urls}>
         <p>Other sizes: </p>
         <a href={urls.small} target="_blank" rel="noopener noreferrer">
@@ -40,11 +49,15 @@ const PhotoCard = ({ photo }) => {
       </div>
       <div className={styles.photoinfo}>
         <div className={styles.likes}>
-          <span className={styles.hearticon}>&#x2764;</span>
+          <span className={styles.hearticon}>
+            <AiOutlineHeart />
+          </span>
           <span>{likes}</span>
         </div>
         <div className={styles.downloads}>
-          <span className={styles.downloadicon}>&#x2B07;</span>
+          <span className={styles.downloadicon}>
+            <AiOutlineDownload />
+          </span>
           <span>{downloads}</span>
         </div>
       </div>
